8eca83f044b9b3c97513b85112cc3aab37414e9b
Jul Jen - Prog <juliajen.prog@gmail.com>
2018-01-16 21:16:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,8 +23,8 @@ class CashRegister
   def apply_discount
 
 binding.pry
-    if self.discount >= 0
 
+    if self.discount >= 0
       self.total = (total * ((100.0-discount.to_f) / 100)).to_i
 
       # "After the discount, the total comes to $800."
