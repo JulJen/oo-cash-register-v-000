e94c0cd796e26e3966c6fbf6f6937567c14e25e5
Jul Jen - Prog <juliajen.prog@gmail.com>
2018-01-16 20:54:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,24 @@ class CashRegister
   end
 
   def add_item (title, price, quantity = 1)
+    # describe '#add_item' do
+    #   it 'accepts a title and a price and increases the total' do
+    #     expect{cash_register.add_item("eggs", 0.98)}.to change{cash_register.total}.by(0.98)
+    #   end
+    #
+    #   it 'also accepts an optional quantity' do
+    #     expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
+    #   end
+    #
+    #   it "doesn't forget about the previous total" do
+    #     cash_register.add_item("Lucky Charms", 4.5)
+    #     expect(cash_register.total).to eq(4.5)
+    #     cash_register.add_item("Ritz Crackers", 5.0)
+    #     expect(cash_register.total).to eq(9.5)
+    #     cash_register.add_item("Justin's Peanut Butter Cups", 2.50, 2)
+    #     expect(cash_register.total).to eq(14.5)
+      end
+    end
 
   end
 
