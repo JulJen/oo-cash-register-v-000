50d68537b0ae2137b87c5c7339878e33af814eab
Jul Jen - Prog <juliajen.prog@gmail.com>
2018-01-16 22:07:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,7 +33,7 @@ class CashRegister
     @items
   end
 
-  def void_last_transaction
-     @total -= @last_transaction
-  end
-end
+#   def void_last_transaction
+#      @total -= @last_transaction
+#   end
+# end
