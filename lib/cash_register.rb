dd8fcec4facdf7ec7380dc3f60b5303c681e3c66
Jul Jen - Prog <juliajen.prog@gmail.com>
2018-01-16 21:17:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -27,7 +27,7 @@ binding.pry
 
       self.total = (total * ((100.0-discount.to_f) / 100)).to_i
 
-      # "After the discount, the total comes to $800."
+      "After the discount, the total comes to $800."
     end
 
   end
